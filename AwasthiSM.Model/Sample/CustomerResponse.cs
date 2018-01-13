e2ec7a8d76f16904db0eed805726f9341e4dcb1c
Anand mohan <anand.mohan.awasthi@outlook.com>
2018-01-14 02:44:26 +0530

    changes

---
+++ AwasthiSM.Model/Sample/CustomerResponse.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AwasthiSM.Model.Sample
+{
+    public class CustomerResponse
+    {
+        public int CustomerId { get; set; }
+        public string CustomerName { get; set; }
+    }
+}
